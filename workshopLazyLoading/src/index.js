38b9a5b961c2a5cbc4d7c5a600c0d8a44e2317c0
fcxmarquez <francisco.marquez.solt@gmail.com>
2022-12-09 17:45:53 -0600

    Creando las imagenes con Javascript

--- workshopLazyLoading/src/index.js
+++ workshopLazyLoading/src/index.js
@@ -1,6 +1,30 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const maximum = 122,
+  minimum = 1;
+const random = () => Math.floor(Math.random() * (maximum - minimum));
 
-console.log('Happy hacking :)')
+const createImageNode = () => {
+  const imageContainer = document.createElement("div");
+  imageContainer.className = "p-4";
+
+  const image = document.createElement("img");
+  image.src = `https://randomfox.ca/images/${random()}.jpg`;
+  image.alt = "fox";
+  image.width = 320;
+  image.className = "mx-auto";
+
+  imageContainer.appendChild(image);
+
+  return imageContainer;
+};
+
+const container = document.getElementById("images");
+const newImage = createImageNode();
+
+const addButton = document.querySelector("button");
+const addImage = () => {
+  const newImage = createImageNode();
+  container.appendChild(newImage);
+};
+addButton.addEventListener("click", addImage);
+
+container.appendChild(newImage);
